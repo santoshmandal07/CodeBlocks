56483f2c345ff4ad90e1639ff4ca8b4da65706e6
Santosh Mandal <139973330+sannn9090@users.noreply.github.com>
2025-10-17 18:44:39 +0530

    header

---
+++ Header/Myheader.h
@@ -0,0 +1,21 @@
+int add(float x, float y)
+{
+    printf("Addition of %.2f and %.2f is %.2f.",x,y,x+y);
+}
+
+int sub(float x, float y)
+{
+    printf("Subtraction of %.2f and %.2f is %.2f.",x,y,x-y);
+}
+
+int mul(float x, float y)
+{
+    printf("Multiplication of %.2f and %.2f is %.2f.",x,y,x*y);
+}
+
+int div(float x, float y)
+{
+
+    printf("Division of %.2f and %.2f is %.2f.",x,y,x/y);
+}
+
---
+++ Header/calculator.c
@@ -0,0 +1,43 @@
+#include"Myheader.h"
+#include<stdio.h>
+
+int main()
+{
+    float a, b;
+    int choice;
+    printf("Enter 1st number: ");
+    scanf("%f",&a);
+    printf("Enter 2nd number: ");
+    scanf("%f",&b);
+
+    printf("1. Addition\n");
+    printf("2. Subtraction\n");
+    printf("3. Multiplication\n");
+    printf("4. Division\n");
+    printf("Enter the choice: ");
+    scanf("%d",&choice);
+
+    switch(choice)
+    {
+         case 1:
+            add(a,b);
+            break;
+
+        case 2:
+            sub(a,b);
+            break;
+
+        case 3:
+            mul(a,b);
+            break;
+
+        case 4:
+            div(a,b);
+            break;
+
+        default:
+            printf("Enter a valid choice!");
+            break;
+    }
+    return 0;
+}
