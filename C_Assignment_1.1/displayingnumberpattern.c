86c7afe268f4caba795656a79f80092233d0a30f
Santosh Mandal <139973330+sannn9090@users.noreply.github.com>
2025-11-10 14:23:34 +0530

    pattern

---
+++ C_Assignment_1.1/displayingnumberpattern.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+    i=5;
+    while (i>=1)
+    {
+        j=1;
+        while (j<=i)
+        {
+            printf("%d",j);
+            j++;
+        }
+        printf("\n");
+        i--;
+    }
+    return 0;
+}
