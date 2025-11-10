2c907ac9cad100aabf0207f9c72ca3a1c8d71db7
Santosh Mandal <139973330+sannn9090@users.noreply.github.com>
2025-11-10 09:47:32 +0530

    new

--- Pointers/addingusingpointer.c
+++ Pointers/addingusingpointer.c
@@ -10,7 +10,7 @@ int main()
     printf("Enter the two numbers: ");
     scanf("%d%d",&a,&b);
     sum = *p + *t;
-    printf("The sum of %d and %d is %d",a,b,sum);
+    printf("The sum of %d and %d is %d\n",a,b,sum);
     printf("The address of a and b is %p %p",p,t);
     return 0;
 }
